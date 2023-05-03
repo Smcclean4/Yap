def40216de2a6c5d0f39a03db384e9a1d8150065
Smcclean4 <lsaudim5@outlook.com>
2023-05-02 19:12:18 -0700

    favicon and background changes for home page

---
+++ yap-chat/src/components/videoplayer.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import dynamic from 'next/dynamic'
+const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
+
+type VideoPlayerTypes = {
+  url: string;
+}
+
+const Videoplayer = ({ url }: VideoPlayerTypes) => {
+  return (
+    <div className="h-screen pointer-events-none">
+      <ReactPlayer url={url} loop={true} playbackRate={0.70} playing={true} controls={false} volume={0} height="100%" width="100%" muted config={{
+        wistia: {
+          options: {
+            endVideoBehavior: "loop",
+            qualityMin: 3840,
+            controlsVisibleOnLoad: false,
+            playButton: false,
+            playPauseNotifier: false,
+            wmode: "transparent",
+            fitStrategy: "cover",
+            plugin: false
+          }
+        }
+      }} />
+      <style jsx>{`
+        .wistia-branding {
+          display: none !important;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+export default Videoplayer
