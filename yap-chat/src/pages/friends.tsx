66c98588b3e8b7048b03a43b8ad3420bf8a1aa4f
Smcclean4 <lsaudim5@outlook.com>
2023-06-15 19:06:07 -0700

    setting up friends page

--- yap-chat/src/pages/friends.tsx
+++ yap-chat/src/pages/friends.tsx
@@ -1,12 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Layout } from '~/components/layout'
 
 const FriendsPage = () => {
 
+  interface FriendInterface {
+    image: string;
+    username: string;
+    heading: string;
+    message: boolean;
+  }
+
+  interface RequestInterface {
+    image: string;
+    username: string;
+    approve: boolean;
+    deny: boolean;
+  }
+
+  const friends: FriendInterface[] = [
+    {
+      image: '',
+      username: '',
+      heading: '',
+      message: false
+    },
+    {
+      image: '',
+      username: '',
+      heading: '',
+      message: false
+    },
+    {
+      image: '',
+      username: '',
+      heading: '',
+      message: false
+    },
+    {
+      image: '',
+      username: '',
+      heading: '',
+      message: false
+    }
+  ]
+
+  const [currentFriends, setCurrentFriends] = useState([])
+  const [currentRequests, setCurrentRequests] = useState([])
+  const [selectedTab, setSelectedTab] = useState(false)
+
+  const handleSelectedTabClick = () => {
+    setSelectedTab(!selectedTab)
+  }
+
   return (
     <Layout>
-      <div className="w-full flex flex-col justify-center items-center mt-28 bg-gray-200">
-        <p>FRIENDS</p>
+      <div className="w-full flex flex-col justify-center items-center mt-28">
+        <div className={`flex flex-row w-full justify-between h-full mt-2 ${selectedTab ? 'bg-gray-200' : 'bg-gray-800'}`}>
+          <p className="bg-gray-200 w-1/2 h-16 text-black text-center flex items-center justify-center text-2xl cursor-pointer hover:text-gray-700" onClick={selectedTab ? undefined : handleSelectedTabClick}>Friends</p>
+          <p className="bg-gray-800 w-1/2 h-16 text-white text-center flex items-center justify-center text-2xl cursor-pointer hover:text-gray-300" onClick={selectedTab ? handleSelectedTabClick : undefined}>Requests</p>
+        </div>
       </div>
     </Layout>
   )
--- yap-chat/src/pages/home.tsx
+++ yap-chat/src/pages/home.tsx
@@ -30,20 +30,20 @@ const HomePage = () => {
 
   return (
     <Layout>
-      <div className="w-full flex flex-col justify-center items-center mt-28 bg-gray-200">
-        <div className="w-3/4 text-3xl leading-10 tracking-wider text-center">
+      <div className="w-full flex flex-col justify-center items-center mt-28 sm:pt-10 bg-gray-200">
+        <div className="w-3/4 text-sm sm:text-lg md:text-xl xl:text-2xl xl:leading-10 tracking-wider text-center">
           <p>Welcome to Yap, the chat application where you can message your friends and send out Yaps - opinionated global messages that allow you to share your thoughts and ideas with the world!</p>
           <br></br>
           <p>With Yap, you can stay connected with your friends and family, have private conversations, and also broadcast your views to a wider audience. Whether you want to share your latest project, your favorite song, or just your opinion on the latest news, Yap is the perfect platform to do it.</p>
         </div>
-        <div className="bg-gray-900/[0.8] mt-16 mb-2 h-72 w-3/5 text-white flex flex-col justify-center items-center text-2xl overflow-auto rounded-lg">
+        <div className="bg-gray-900/[0.8] mt-16 mb-2 h-72 w-5/6 lg:w-3/5 text-white flex flex-col justify-center items-center text-2xl overflow-auto rounded-lg">
           <div className="py-5 h-full w-4/5">
-            <p className="underline text-center text-4xl mb-6"><b>Recent News</b></p>
+            <p className="underline text-center text-xl sm:text-2xl md:text-3xl lg:text-4xl mb-6"><b>Recent News</b></p>
             {homeUpdates?.map((content: { heading: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; description: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; }, idx: React.Key | null | undefined) => {
               return (
                 <div key={idx}>
-                  <p className="underline text-3xl"><i>{content.heading}</i></p>
-                  <p className="py-6">{content.description}</p>
+                  <p className="underline text-md md:text-2xl"><i>{content.heading}</i></p>
+                  <p className="py-6 text-sm md:text-xl">{content.description}</p>
                 </div>
               )
             })}
