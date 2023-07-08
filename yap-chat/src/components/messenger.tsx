7f149de5b3bf7197c6c7fb5c227bc9fd43280791
Smcclean4 <lsaudim5@outlook.com>
2023-07-07 21:14:21 -0700

    Update messenger.tsx

--- yap-chat/src/components/messenger.tsx
+++ yap-chat/src/components/messenger.tsx
@@ -2,6 +2,8 @@ import { faCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from 'react'
 import { MessageInfoInterface } from '~/pages/friends';
+import { MessageModal } from '~/modals/message';
+import { useModal } from '~/hooks/useModal';
 
 interface MessengerInterface {
   messengeruser?: MessageInfoInterface;
@@ -9,8 +11,20 @@ interface MessengerInterface {
 
 export const ChatMessenger = ({ messengeruser }: MessengerInterface) => {
   // when chat is open.. make new chat messenger in sidebar nav.
-  // when messenger open button is clicked open modal with corresponding chat.
+  // when messenger open button is clicked open modal with corresponding chats
   const [chats, setChats]: Array<any> = useState([])
+  const [messengerUser, setMessengerUser]: any = useState('')
+
+  const { isShowing, toggle } = useModal();
+
+  const onMessageSend = () => {
+    console.log('get money')
+  }
+
+  const onMessage = (idx: React.Key) => {
+    setMessengerUser(chats[idx].username)
+    toggle()
+  }
 
   const itemExists = (username: string | undefined, item: { username: any; }[]) => {
     return item.some((chat: { username: any; }) => {
@@ -43,9 +57,10 @@ export const ChatMessenger = ({ messengeruser }: MessengerInterface) => {
 
   return (
     <div className="flex flex-col flex-grow mt-32 overflow-scroll no-scrollbar overflow-y-auto">
+      <MessageModal isShowing={isShowing} hide={toggle} sendmessage={onMessageSend} messages={'user messages here'} user={messengerUser} />
       {chats?.map((chats: { username: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; online: any; message: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; }, idx: React.Key) => {
         return (
-          <div key={idx} className="text-white bg-gray-900 w-full py-3 h-min border-b-2 border-gray-300">
+          <div key={idx} className="text-white bg-gray-900 w-full py-3 h-min border-b-2 border-gray-300 cursor-pointer" onClick={() => onMessage(idx)}>
             <div className="flex flex-row justify-around items-center">
               <p className="text-xl">{chats?.username}</p>
               <FontAwesomeIcon className="border-2 border-gray-100 rounded-full" icon={faCircle} color={chats?.online ? 'limegreen' : 'gray'} size="sm" />
