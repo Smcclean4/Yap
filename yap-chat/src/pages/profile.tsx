0193f1c0ceecaf9bce5abb8b56b5d2c6177de5b6
Smcclean4 <lsaudim5@outlook.com>
2023-07-13 19:30:01 -0700

    Update profile.tsx

--- yap-chat/src/pages/profile.tsx
+++ yap-chat/src/pages/profile.tsx
@@ -63,6 +63,7 @@ const ProfilePage = () => {
 
   useEffect(() => {
     localStorage.setItem('profileData', JSON.stringify(profileData))
+    console.log(session?.user)
   }, [profileData])
 
   return (
@@ -70,11 +71,11 @@ const ProfilePage = () => {
       <SidebarNav user={session?.user.email} />
       <div className="w-full flex justify-center items-center mt-28 bg-gray-200 flex-wrap overflow-scroll no-scrollbar overflow-y-auto py-6">
         <div className="w-3/4 text-center flex flex-col flex-wrap justify-center items-center">
-          <div className="mb-8 flex flex-col items-center">
+          <div className=" flex flex-col items-center">
             <div className="h-48 w-48 border-2 border-white bg-white flex justify-center items-center rounded-full overflow-hidden">
               <Image src={!session?.user.image ? "/ezgif.com-webp-to-jpg.jpg" : session?.user.image!} alt='' height="200" width="200" priority />
             </div>
-            {editMode && <input className="my-6 ml-32" type="file" onChange={handleImageUpload} name="image" accept="image/png, image/jpg, image/gif" />}
+            {editMode && <input className="my-4 ml-32" type="file" onChange={handleImageUpload} name="image" accept="image/png, image/jpg, image/gif" />}
           </div>
           <div className="my-2 w-full">
             {editMode ? (
@@ -84,7 +85,7 @@ const ProfilePage = () => {
               </div>
             ) : <p className="text-xl"><b>@{profileData.username === undefined ?? "" ? <span className="text-red-500">Set Your Username</span> : profileData.username}</b></p>}
           </div>
-          <div className="my-2 w-full flex flex-col justify-center items-center">
+          <div className="w-full flex flex-col justify-center items-center">
             {editMode ? (
               <div className="w-full">
                 <p className="text-xl underline mb-2">Heading</p>
@@ -97,11 +98,11 @@ const ProfilePage = () => {
               </div>
             )}
           </div>
-          <div className="my-4 w-full h-48 flex flex-col justify-center items-center">
+          <div className="w-full flex flex-col justify-center items-center">
             {editMode ? (
-              <div className="w-full h-full">
+              <div className="w-full">
                 <p className="text-xl underline mb-2">Bio</p>
-                <textarea className="w-2/3 h-full p-2 resize-none" placeholder="Tell us about yourself!" maxLength={350} onChange={onEditChanges} name="bio" value={profileData.bio ?? ""} />
+                <textarea className="w-2/3 h-36 mb-2 p-2 resize-none" placeholder="Tell us about yourself!" maxLength={350} onChange={onEditChanges} name="bio" value={profileData.bio ?? ""} />
               </div>
             ) : (
               <div className="w-2/3 max-h-48">
@@ -110,7 +111,7 @@ const ProfilePage = () => {
               </div>
             )}
           </div>
-          <div className="flex flex-row justify-evenly mt-12 w-full">
+          <div className="flex flex-row justify-evenly w-full">
             {editMode &&
               <button className="text-xl text-white bg-blue-500 py-2 px-6 rounded-full" onClick={handleEdit}>Cancel</button>}
             {editMode ? (
