ddefc94078d4df761424d0ca28ff58f094ca7189
Smcclean4 <lsaudim5@outlook.com>
2023-07-19 19:33:46 -0700

    fixing yap styles for smaller screens

--- yap-chat/src/pages/yaps.tsx
+++ yap-chat/src/pages/yaps.tsx
@@ -28,7 +28,6 @@ const YapsPage = () => {
   const { data: session } = useSession();
 
   const [yaps, setYaps]: Array<any> = useState([])
-  const [yapError, setYapError] = useState('')
   const [updateMessage, setUpdateMessage] = useState('')
   const [trueEditFalseDelete, setTrueEditFalseDelete] = useState(false)
   const [deleteInfo, setDeleteInfo] = useState<DeleteInterface>({ deleteUser: '', deleteMessage: '' })
@@ -64,7 +63,7 @@ const YapsPage = () => {
     if (session) {
       setPersonalYap({ ...personalYap, user: session?.user.email })
     } else {
-      setYapError('User is not defined')
+      toast.error('User is not defined.')
     }
   }
 
@@ -145,10 +144,10 @@ const YapsPage = () => {
         ) : (
           <DeleteModal isShowing={isShowing} hide={toggle} deleteitem={deleteItem} item={'this Yap'} theme={'bg-white'} text={'text-black'} />
         )}
-        <div className="flex flex-row justify-center h-full w-full flex-wrap overflow-scroll no-scrollbar overflow-y-auto content-start">
+        <div className="flex justify-center h-full w-full flex-wrap overflow-scroll no-scrollbar overflow-y-auto content-start">
           {yaps?.map((allYaps: { message: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined }, idx: React.Key) => {
             return (
-              <div className="h-64 w-96 max-w-5xl bg-gray-800 text-white m-8 flex flex-col rounded-tr-3xl rounded-tl-3xl rounded-bl-3xl" key={idx}>
+              <div className="w-full h-fit max-w-xs bg-gray-800 text-white m-8 flex flex-col rounded-tr-3xl rounded-tl-3xl rounded-bl-3xl" key={idx}>
                 <div className="flex flex-row items-center justify-between">
                   <Image className="m-4" src={'/ezgif.com-webp-to-jpg.jpg'} alt={''} height="50" width="50" />
                   {session?.user.email === yaps[idx].user && <FontAwesomeIcon className="m-4 cursor-pointer" onFocus={() => {
@@ -162,7 +161,7 @@ const YapsPage = () => {
                     </div>
                   ) : ""}
                 </div>
-                <p className="text-xl text-center">{allYaps.message}</p>
+                <p className="text-xl text-left pl-4">{allYaps.message}</p>
                 <div className="flex justify-end items-end flex-grow m-4">
                   <FontAwesomeIcon className="m-2 cursor-pointer" icon={faHeart} onClick={() => onLike(idx)} color={yaps[idx].liked ? "red" : "white"} size="xl" />
                   <FontAwesomeIcon className="m-2 cursor-pointer" icon={faUserPlus} onClick={() => onFriend(idx)} color={yaps[idx].friend ? "skyblue" : "white"} size="xl" />
@@ -173,7 +172,6 @@ const YapsPage = () => {
         </div>
         <div className="h-auto w-full flex flex-row justify-center items-end">
           <div className="bg-gray-300 text-black p-6 w-full flex flex-col justify-center text-center">
-            {yapError && <p className="text-white bg-red-600 w-full max-w-md self-center text-xl p-2 rounded-tl-xl rounded-tr-xl">{yapError}</p>}
             <div className="flex flex-row justify-center">
               <input className="p-2 rounded-tl-full rounded-bl-full w-full max-w-3xl" type="text" name="message" placeholder="Enter your message here..." value={personalYap.message} onFocus={setUser} maxLength={125} onChange={handleYapDisplay} onKeyDown={(e) => e.key === "Enter" && handleYapSend()} />
               <button className="px-4 py-2  text-white bg-blue-400 hover:bg-blue-500 rounded-tr-full rounded-br-full" type="submit" onClick={handleYapSend}>Send</button>
