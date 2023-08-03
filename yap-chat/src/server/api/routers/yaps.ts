08485f4742e954231a40ada908ca6b800d2f5a4a
Smcclean4 <lsaudim5@outlook.com>
2023-08-02 20:23:58 -0700

    working on yaps routes

---
+++ yap-chat/src/server/api/routers/yaps.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  publicProcedure,
+} from "~/server/api/trpc";
+
+export const homeUpdatesRouter = createTRPCRouter({
+  getHomeUpdates: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.homeUpdates.findMany()
+  })
+});
