dca2bd0d27f4b32bfcc257618e4603b1b098f1b6
Smcclean4 <lsaudim5@outlook.com>
2024-01-10 21:13:59 -0800

    update

--- yap-chat/src/pages/yaps.tsx
+++ yap-chat/src/pages/yaps.tsx
@@ -20,7 +20,6 @@ dayjs.extend(relativeTime)
 const YapsPage = () => {
 
   interface YapInterface {
-    message: string;
     likes: string[];
     user: string | undefined | null;
     options: boolean;
@@ -33,12 +32,11 @@ const YapsPage = () => {
 
   const { data: session } = useSession();
 
-  // const [yaps, setYaps]: Array<any> = useState([])
+  const [allYaps, setAllYaps]: Array<any> = useState([])
   const [updateMessage, setUpdateMessage] = useState('')
   const [trueEditFalseDelete, setTrueEditFalseDelete] = useState(false)
   const [deleteInfo, setDeleteInfo] = useState<DeleteInterface>({ deleteUser: '', deleteMessage: '' })
   const [personalYap, setPersonalYap] = useState<YapInterface>({
-    message: '',
     likes: [],
     user: '',
     options: false
@@ -47,9 +45,9 @@ const YapsPage = () => {
   const { isShowing, toggle } = useModal();
 
   const onOption = (idx: React.Key) => {
-    // setYaps((state: { options: boolean }[]) => state?.map((yap: { options: boolean }, i: React.Key) => {
-    //   return i === idx ? { ...yap, options: !yap.options } : yap
-    // }))
+    setAllYaps((state: { options: boolean }[]) => state?.map((yap: { options: boolean }, i: React.Key) => {
+      return i === idx ? { ...yap, options: !yap.options } : yap
+    }))
   }
 
   const setUser = () => {
@@ -61,12 +59,12 @@ const YapsPage = () => {
   }
 
   const currentDeleteData = (idx: React.Key) => {
-    // setDeleteInfo({ deleteUser: yaps[idx].user, deleteMessage: yaps[idx].message })
+    // setDeleteInfo({ deleteUser: AllYaps[idx].user, deleteMessage: AllYaps[idx].message })
   }
 
   const deleteItem = () => {
-    // setYaps((state: any[]) => state.filter((yap: YapInterface, i: React.Key) => {
-    //   if (yaps[i].user === deleteInfo.deleteUser && yaps[i].message === deleteInfo.deleteMessage) {
+    // setAllYaps((state: any[]) => state.filter((yap: YapInterface, i: React.Key) => {
+    //   if (AllYaps[i].user === deleteInfo.deleteUser && AllYaps[i].message === deleteInfo.deleteMessage) {
     //     return false
     //   } else {
     //     return yap
@@ -89,8 +87,8 @@ const YapsPage = () => {
       toast.error('Message cannot be empty!')
       return
     }
-    // setYaps((state: { message: string }[]) => state?.map((yap: { message: string }, i: React.Key) => {
-    //   return yaps[i].user === deleteInfo.deleteUser && yaps[i].message === deleteInfo.deleteMessage ? { ...yap, message: updateMessage } : yap
+    // setAllYaps((state: { message: string }[]) => state?.map((yap: { message: string }, i: React.Key) => {
+    //   return AllYaps[i].user === deleteInfo.deleteUser && AllYaps[i].message === deleteInfo.deleteMessage ? { ...yap, message: updateMessage } : yap
     // }))
     setUpdateMessage('')
     toast.success('Yap updated!')
@@ -112,26 +110,26 @@ const YapsPage = () => {
     setPersonalYap({ ...personalYap, [input.name]: input.value })
   }
 
-  const handleYapSend = () => {
-    if (personalYap.message === '') {
-      toast.error('Please type a message!')
-      return
-    }
-    // setYaps([...yaps, personalYap])
-    setPersonalYap({ ...personalYap, message: '' })
-  }
+  // const handleAllYapend = () => {
+  //   if (personalYap.message === '') {
+  //     toast.error('Please type a message!')
+  //     return
+  //   }
+  //   // setAllYaps([...allYaps, personalYap])
+  //   setPersonalYap({ ...personalYap, message: '' })
+  // }
 
   // useEffect(() => {
   //   const yapsStorage = JSON.parse(localStorage.getItem('yapsData') || '[]')
   //   if (yapsStorage) {
-  //     setYaps(yapsStorage)
+  //     setAllYaps(yapsStorage)
   //   }
   // }, [])
 
   // useEffect(() => {
-  //   localStorage.setItem('yapsData', JSON.stringify(yaps))
-  //   console.log(yaps)
-  // }, [yaps])
+  //   localStorage.setItem('yapsData', JSON.stringify(allYaps))
+  //   console.log(allYaps)
+  // }, [allYaps])
 
   // sets users message and adds it to post
   const [userMessage, setUserMessage] = useState('')
@@ -151,13 +149,20 @@ const YapsPage = () => {
 
   if (!session) return null
 
-  const AllYaps = () => {
+  const DisplayAllYaps = () => {
     // handle all yaps display and current user likes
     const { data: yaps, isLoading: loadingYaps } = api.yap.getAllYaps.useQuery()
     const { data: uniqueYaps } = api.yap.findSpecificUserLikes.useQuery({ text: String(session?.user.email) })
 
     if (loadingYaps) return <LoadingPage />
+
+    // figure out how to manipulate options of each with "onOptions"
+    useEffect(() => {
+      setAllYaps(yaps)
+    }, [])
+
     console.log(yaps)
+    console.log(allYaps)
 
     return (
       <>
@@ -168,10 +173,12 @@ const YapsPage = () => {
               <div className="flex items-center justify-between">
                 <Image className="m-4 rounded-full" src={'/ezgif.com-webp-to-jpg.jpg'} alt={''} height="50" width="50" />
                 <p className="text-md md:text-lg font-extralight italic text-gray-300"><span className="font-extralight">{` • ${dayjs(allYaps.createdAt).fromNow()}`}</span></p>
-                <div className="flex items-center justify-end pr-2 relative" onBlur={() => optionToggle({ id: allYaps.id })} tabIndex={0}>
+                <div className="flex items-center justify-end pr-2 relative" onBlur={() => onOption(idx)} tabIndex={0}>
                   {session?.user.email === allYaps.user && <FontAwesomeIcon className="m-4 cursor-pointer" icon={faEllipsis} size="xl" onFocus={() =>
                     // prevent default?
-                    optionToggle({ id: allYaps.id })} tabIndex={0} />}
+                    // optionToggle({ id: allYaps.id })
+                    onOption(idx)
+                  } tabIndex={0} />}
                   {allYaps.options && session?.user.email && (
                     <div className="absolute text-center flex flex-col border-2 w-28 bg-gray-500 border-none text-lg text-white">
                       <button onMouseDown={onEdit}>Edit</button>
@@ -202,7 +209,7 @@ const YapsPage = () => {
           <DeleteModal isShowing={isShowing} hide={toggle} deleteitem={deleteItem} item={'this Yap'} theme={'bg-white'} text={'text-black'} />
         )}
         <div className="flex justify-center h-full w-full flex-wrap overflow-scroll no-scrollbar overflow-y-auto content-start">
-          <AllYaps />
+          <DisplayAllYaps />
         </div>
         <div className="h-auto w-full flex flex-row justify-center items-end">
           <div className="bg-gray-300 text-black p-6 w-full flex flex-col justify-center text-center">
