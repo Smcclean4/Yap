2ef376b6fec2c509e3f6274ffd6a22e508e8b9ec
Smcclean4 <lsaudim5@outlook.com>
2024-03-19 20:06:57 -0700

    update to home screen design - more mobile friendly

--- yap-chat/src/pages/home.tsx
+++ yap-chat/src/pages/home.tsx
@@ -23,7 +23,7 @@ const HomePage = () => {
       <>
         {data?.map((content, idx: React.Key | null | undefined) => {
           return (
-            <div className="h-screen" key={idx}>
+            <div key={idx}>
               <p className="text-md md:text-2xl"><i className="underline">{content.heading}</i><span className="font-extralight">{` • ${dayjs(content.createdAt).fromNow()}`}</span></p>
               <p className="py-6 text-sm md:text-xl">{content.description}</p>
             </div>
@@ -36,17 +36,19 @@ const HomePage = () => {
   return (
     <Layout>
       <SidebarNav user={session?.user.email} />
-      <div className="w-full flex flex-col justify-center items-center mt-28 sm:pt-10 bg-gray-200 h-auto overflow-scroll">
-        <div className="w-3/4 text-sm sm:text-lg md:text-xl xl:text-2xl xl:leading-10 tracking-wider text-center">
+      <div className="w-full flex flex-col md:flex-row justify-around items-center mt-28 lg:pb-20 md:pt-24 sm:pt-32 bg-gray-200 overflow-scroll h-auto">
+        <div className="w-4/5 md:w-2/5 text-sm sm:text-lg md:text-xl xl:text-2xl xl:leading-10 tracking-wider text-center">
           <p>Welcome to Yap, the chat application where you can message your friends and send out Yaps - opinionated global messages that allow you to share your thoughts and ideas with the world!</p>
           <br></br>
           <p>With Yap, you can stay connected with your friends and family, have private conversations, and also broadcast your views to a wider audience. Whether you want to share your latest project, your favorite song, or just your opinion on the latest news, Yap is the perfect platform to do it.</p>
         </div>
-        <div className="text-center text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl mt-12 font-bold bg-blue-500 px-10 py-4 rounded-tr-2xl rounded-tl-2xl">
-          <p className="underline">Recent News</p>
-        </div>
-        <div className="bg-gray-900/[0.8] mb-2 h-72 w-5/6 lg:w-3/5 text-white flex flex-col justify-center items-center text-2xl overflow-auto rounded-lg">
-          <RecentNews />
+        <div className="flex flex-col w-3/4 md:w-2/5 h-full">
+          <div className="text-center text-white text-xl sm:text-2xl md:text-3xl lg:text-4xl mt-12 font-bold bg-blue-500 px-10 py-4 rounded-tr-2xl rounded-tl-2xl">
+            <p className="underline">Recent News</p>
+          </div>
+          <div className="bg-gray-900/[0.8] mb-2 w-full h-full text-white flex flex-col text-2xl overflow-auto rounded-bl-lg rounded-br-lg p-4">
+            <RecentNews />
+          </div>
         </div>
       </div>
     </Layout>
