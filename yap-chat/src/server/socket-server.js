1bb57b59f2127a327fe7bc871e41d4ddaf09260f
Smcclean4 <lsaudim5@outlook.com>
2024-11-12 22:41:41 -0800

    add logic for emitting and triggering

--- yap-chat/src/server/socket-server.js
+++ yap-chat/src/server/socket-server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { createServer } from "node:http";
+import { isBooleanObject } from "node:util/types";
 import { Server } from "socket.io";
 
 const app = express();
@@ -20,6 +21,13 @@ io.on("connection", (socket) => {
   console.log("a user connected");
   socket.on("disconnect", () => {
     console.log("a user disconnected");
+  });
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+    console.log("chat message: " + msg);
   });
 });
 
