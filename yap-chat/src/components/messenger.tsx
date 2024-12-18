b97b97305dda19649cbb07699014c8b3d34294fa
Smcclean4 <lsaudim5@outlook.com>
2024-12-17 20:35:52 -0800

    created messages schema

--- yap-chat/src/components/messenger.tsx
+++ yap-chat/src/components/messenger.tsx
@@ -52,7 +52,6 @@ export const ChatMessenger = ({ messengeruser, trigger }: MessengerInterface) =>
 
   const triggerMessage = () => {
     setMessengerUser(messengeruser?.name)
-    // establish connection here
     toggle()
   }
 
