9e0f8d2a5d129566f9d6eafbef7235ab85bc5d46
Smcclean4 <lsaudim5@outlook.com>
2026-03-13 12:26:56 -0700

    animation for sidebar complete

--- yap-chat/src/components/sidebar.tsx
+++ yap-chat/src/components/sidebar.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { LogoutForm } from './logout'
 import { ChatMessenger } from './messenger'
 import { UserInfoInterface } from '~/pages/friends';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 interface SidebarNavInterface {
   user: string | undefined | null;
@@ -11,13 +13,22 @@ interface SidebarNavInterface {
 
 export const SidebarNav = ({ user, userinfo, triggermessage }: SidebarNavInterface) => {
 
+  const [hiddenTabIndicator, setHiddenTabIndicator] = useState(true);
+
   return (
-    <div className="sm:flex flex-col justify-end w-min hidden h-full sm:h-auto absolute sm:static xl:w-1/6 text-center bg-gray-400">
-      <ChatMessenger messengeruser={userinfo} trigger={triggermessage} />
-      <div className="p-6">
-        <LogoutForm />
-        <p>Logged in as {user}</p>
+    <>
+      <div className={`flex flex-col justify-end w-min h-full sm:h-auto absolute sm:static xl:w-1/6 text-center bg-gray-400 ${hiddenTabIndicator ? '-translate-x-full' : 'translate-x-1/6'} sm:translate-x-0 transition-transform duration-300 ease-in-out`}>
+        <ChatMessenger messengeruser={userinfo} trigger={triggermessage} />
+        <div className="p-6">
+          <LogoutForm />
+          <p>Logged in as {user}</p>
+        </div>
       </div>
-    </div>
+      <div className="sm:hidden flex flex-col justify-center items-center h-full z-50">
+        <div className="h-auto bg-blue-500 rounded-tr-md rounded-br-md hover:cursor-pointer" onClick={() => setHiddenTabIndicator(!hiddenTabIndicator)}>
+          <FontAwesomeIcon icon={hiddenTabIndicator ? faArrowRight : faArrowLeft} size="2xl" className="m-2 text-white" />
+        </div>
+      </div>
+    </>
   )
 }
