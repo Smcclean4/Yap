8338597875d81bf2112fcff7c9231c4ec891a74e
Smcclean4 <lsaudim5@outlook.com>
2026-04-15 21:19:55 -0700

    update to header: iphone

--- yap-chat/src/components/header.tsx
+++ yap-chat/src/components/header.tsx
@@ -4,6 +4,9 @@ export const Header = () => {
   return (
     <Head>
       <title>Yap Chat</title>
+      <meta name="theme-color" content="#000000" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="black" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
